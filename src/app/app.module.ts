a04e9ed9afc248920f29e9d95f6e303d138f1304
kiruthikayini <skiruthikayini@greatergoods.com>
2025-12-28 21:27:24 +0530

    Add PrimeNG and PrimeIcons integration:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +10,19 @@ import { HomeComponent } from './pages/home/home.component';
 import { NotesComponent } from './pages/notes/notes.component';
 import { TodoComponent } from './pages/todo/todo.component';
 import { SettingsComponent } from './pages/settings/settings.component';
+
+// PrimeNG Modules
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { CardModule } from 'primeng/card';
+import { PanelModule } from 'primeng/panel';
+import { DividerModule } from 'primeng/divider';
+import { MessageModule } from 'primeng/message';
+import { ToastModule } from 'primeng/toast';
+import { RippleModule } from 'primeng/ripple';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FileUploadModule } from 'primeng/fileupload';
 
 @NgModule({
   declarations: [
@@ -19,8 +34,22 @@ import { SettingsComponent } from './pages/settings/settings.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    // PrimeNG Modules
+    ButtonModule,
+    InputTextModule,
+    InputTextareaModule,
+    CardModule,
+    PanelModule,
+    DividerModule,
+    MessageModule,
+    ToastModule,
+    RippleModule,
+    CheckboxModule,
+    FileUploadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/constants/app.constants.ts
+++ src/app/constants/app.constants.ts
@@ -9,9 +9,31 @@ export class AppConstants {
     EMPTY_STATE_MESSAGE: 'No notes yet. Add your first note above!'
   };
 
+  // Todo Component Strings
+  static readonly TODO = {
+    PAGE_TITLE: 'To-Do List',
+    TASK_PLACEHOLDER: 'Add a new task...',
+    ADD_TASK_BUTTON: 'Add Task',
+    DELETE_BUTTON: 'Delete',
+    EMPTY_STATE_MESSAGE: 'No tasks yet. Add your first task above!',
+    FILTER_ALL: 'All',
+    FILTER_ACTIVE: 'Active',
+    FILTER_COMPLETED: 'Completed'
+  };
+
+  // Settings Component Strings
+  static readonly SETTINGS = {
+    PAGE_TITLE: 'Settings',
+    CLEAR_DATA: 'Clear All Data',
+    EXPORT_DATA: 'Export Data',
+    IMPORT_DATA: 'Import Data',
+    THEME_LABEL: 'Theme'
+  };
+
   // LocalStorage Keys
   static readonly STORAGE_KEYS = {
-    NOTES: 'notes'
+    NOTES: 'notes',
+    TODOS: 'todos'
   };
 }
 
